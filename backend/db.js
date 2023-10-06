2813983d75b07b384f5a5ac1c9fe9e8f59a4b688
Kartikey Shrivastav <ks12.nd@gmail.com>
2023-10-06 14:38:44 +0530

    Error Handling and Error Display

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI= "mongodb://localhost:27017/?directConnection=true&readPreference=primary"
+const mongoURI= "mongodb://localhost:27017/iNotebook?directConnection=true&readPreference=primary"
 
 const connectToMongo=async ()=>{
    await mongoose.connect(mongoURI);
