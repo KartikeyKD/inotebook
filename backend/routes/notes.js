187db5f1c6efade0ffd3a4be5c0cab30237944e6
Kartikey Shrivastav <ks12.nd@gmail.com>
2023-10-07 01:42:36 +0530

    Delete and Update Notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -46,7 +46,7 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
 //     res.status(500).send("Some Error occured");       
 // }
 // });
-
+//ROUTE 2: Add notes  using: POST "http://localhost:6000/api/notes/addnote". Login Required
 router.post('/addnote', fetchuser, [
     body('title', 'Enter a valid title').isLength({ min: 3 }),
     body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),], async (req, res) => {
@@ -72,4 +72,42 @@ router.post('/addnote', fetchuser, [
         }
     })
 
-module.exports = router;
+
+//ROUTE 3: Update notes  using: PUT "http://localhost:6000/api/notes/updatenote". Login Required
+router.put('/updatenote/:id', fetchuser, async (req,res)=>{
+     const {title, description, tag} = req.body;
+    //creating a newNote object 
+    const newNote = {};
+    if(title){newNote.title=title};
+    if(description){newNote.description=description};
+    if(tag){newNote.tag=tag};
+    
+    //Finding and updating the requested note 
+     let note = await Note.findById(req.params.id);
+     if(!note){
+       return res.status(404).send("Not Found")
+     }
+
+     if(note.user.toString()!==req.user.id){return res.status(401).send("Not authorised")};
+note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
+res.json({note});
+
+    });
+
+//ROUTE 4: Deleting notes  using: DELETE "http://localhost:6000/api/notes/deletenote". Login Required
+
+router.delete('/deletenote/:id', fetchuser, async (req,res)=>{
+     const {title, description, tag} = req.body;
+    
+     //Finding and Deleting the requested note 
+     let note = await Note.findById(req.params.id);
+     if(!note){
+       return res.status(404).send("Not Found")
+     }
+//Checking user credentials
+     if(note.user.toString()!==req.user.id){return res.status(401).send("Not authorised")};
+note = await Note.findByIdAndDelete(req.params.id);
+res.json({note:note, "message":"The Note is deleted"});
+
+    });
+ module.exports = router;
