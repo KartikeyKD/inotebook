058b80d3f61d9e27e0cc555b85b91da33ef831d8
Kartikey Shrivastav <ks12.nd@gmail.com>
2023-10-06 19:49:22 +0530

    Login Authentication added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 
 const JWT_SECRET = "IamGoodBoy@1";
 
-// Create a User using: POST "/api/auth/createuser". No login required
+//ROUTE 1: Create a User using: POST "/api/auth/createuser". No login required
 router.post(
   "/createuser",
   [
@@ -47,8 +47,8 @@ router.post(
       };
 
       const authtoken = jwt.sign(data, JWT_SECRET);
-      
-      res.json({authtoken});
+
+      res.json({ authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some Error occured");
@@ -56,4 +56,39 @@ router.post(
   }
 );
 
+//ROUTE 2: Authenticate a User using: POST "http://localhost:6000/api/auth/login". No login required
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password can not be blank").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user =await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: "Credentials did not match" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res.status(400).json({ error: "Credentials did not match" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some Error occured");
+    }
+  }
+);
 module.exports = router;
